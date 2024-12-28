0c76bbe7b55592fa3659e18ace749ae35d21c907
Pankaj Chejara <pankajchejara23@gmail.com>
2024-12-28 07:56:52 +0200

    Update apply_trimmomatic.sh

--- apply_trimmomatic.sh
+++ apply_trimmomatic.sh
@@ -4,7 +4,7 @@
 
 samples=()
 
-for filename in ./raw_files/*.fastq.gz; do
+for filename in ./raw_data/*.fastq.gz; do
     base=$(basename "$filename" .fastq.gz)
     nf=$(echo $base | sed -e 's/.......$//');
     if ! [[ ${samples[@]} =~ $nf ]]
@@ -13,10 +13,9 @@ for filename in ./raw_files/*.fastq.gz; do
     fi
   done
 
-
 for sample in "${samples[@]}"; do
-  forward="${sample}_R1_001.fastq.gz"
-  backward="${sample}_R2_001.fastq.gz"
-  baseout="${sample}.fastq.gz"
-  printf "\n trimmomatic -PE -threads 20 $forward $backward -baseout $baseout  CROP:200"
+  forward="./raw_data/${sample}_R1_001.fastq.gz"
+  backward="./raw_data/${sample}_R2_001.fastq.gz"
+  baseout="./trimm_outputs/${sample}.fastq.gz"
+  trimmomatic PE -threads 20 $forward $backward -baseout $baseout CROP:200"
 done;
