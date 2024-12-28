38b60bd2474a703b19c5f1a46fce6de9c37fd5fc
Pankaj Chejara <pankajchejara23@gmail.com>
2024-12-28 08:20:35 +0200

    Create create_manifest.sh

---
+++ create_manifest.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+# author: Pankaj chejara
+# Script to create manifest file for qiime2
+
+samples=()
+
+for filename in ./raw_files/*.fastq.gz; do
+    base=$(basename "$filename" .fastq.gz)
+    nf=$(echo $base | sed -e 's/.......$//');
+    if ! [[ ${samples[@]} =~ $nf ]]
+    then
+      samples+=("$nf");
+    fi
+  done
+
+echo "sample-id,absolute-filepath,direction" >> manifest.csv
+
+for sample in "${samples[@]}"; do
+  forward="$PWD/trimm_outputs/${sample}_1P.fastq.gz"
+  backward="$PWD/trimm_outputs/${sample}_2P.fastq.gz"
+  sampleid=$(echo $sample | sed -e 's/_.*//')
+  echo "$sampleid,$forward,forward" >> manifest.csv
+  echo "$sampleid,$backward,backward" >> manifest.csv
+done;
